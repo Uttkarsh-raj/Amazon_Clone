6699909ef4df353a36568d1d601bddf8b9aa9d5a
Uttkarsh-raj <uttkarsh.raj.jsr@gmail.com>
2023-01-07 12:17:31 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Amazon',
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor,
+        ),
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+        ),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: const Text('Hello'),
+        ),
+        body: const Center(
+          child: Text(
+            'Flutter Demo Home Page',
+          ),
+        ),
+      ),
+    );
+  }
+}
